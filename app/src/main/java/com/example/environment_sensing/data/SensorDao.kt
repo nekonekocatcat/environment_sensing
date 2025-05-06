498e88f98cb076fb5dbd8dfd5fea8e3e1a4ccdea
nekonekocatcat <n0k2s0m1+github@gmail.com>
2025-05-06 21:56:03 +0900

    DB上に保存したデータを画面上に表示

--- app/src/main/java/com/example/environment_sensing/data/SensorDao.kt
+++ app/src/main/java/com/example/environment_sensing/data/SensorDao.kt
@@ -3,6 +3,7 @@ package com.example.environment_sensing.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SensorDao {
@@ -11,4 +12,7 @@ interface SensorDao {
 
     @Query("SELECT * FROM SensorRecord ORDER BY timestamp DESC")
     suspend fun getAll(): List<SensorRecord>
+
+    @Query("SELECT * FROM sensorrecord ORDER BY timestamp DESC")
+    fun getAllFlow(): Flow<List<SensorRecord>>
 }
