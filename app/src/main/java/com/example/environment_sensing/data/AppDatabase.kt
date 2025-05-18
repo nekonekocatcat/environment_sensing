3e37e783d7a41a3cb2c2e189169e1ad1630cece6
nekonekocatcat <n0k2s0m1+github@gmail.com>
2025-05-18 23:16:35 +0900

    生データ保存用と平均処理をしたデータの保存用のDBを分けた

--- app/src/main/java/com/example/environment_sensing/data/AppDatabase.kt
+++ app/src/main/java/com/example/environment_sensing/data/AppDatabase.kt
@@ -5,9 +5,11 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [SensorRecord::class], version = 1)
+@Database(entities = [SensorRecord::class, SensorRawRecord::class],
+    version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun sensorDao(): SensorDao
+    abstract fun sensorRawDao(): SensorRawDao
 
     companion object {
         @Volatile
@@ -19,7 +21,7 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "sensor_database"
-                ).build()
+                ).fallbackToDestructiveMigration().build()
                 INSTANCE = instance
                 instance
             }
