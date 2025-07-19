008fef0bb17ae5c280621c7c5c0ad65d6726c294
nekonekocatcat <n0k2s0m1+github@gmail.com>
2025-07-19 19:13:21 +0900

    データセット見直し

--- app/src/main/java/com/example/environment_sensing/NormalEnvironmentChecker.kt
+++ app/src/main/java/com/example/environment_sensing/NormalEnvironmentChecker.kt
@@ -9,21 +9,21 @@ object NormalEnvironmentChecker {
 
     private val environments = listOf(
         NormalEnvironment("静かめ快適環境") {
-            it.temperature in 20.0..26.0 &&
+            it.temperature in 22.0..26.0 &&
                     it.noise < 60.0
         },
         NormalEnvironment("涼しめ明るい環境") {
-            it.temperature in 15.0..25.0 &&
-                    it.light > 1000 &&
+            it.temperature in 15.0..20.0 &&
+                    it.light > 300 &&
                     it.noise > 60.0
         },
-        NormalEnvironment("高湿度×涼しい環境") {
-            it.temperature < 26.0 &&
-                    it.humidity > 70.0
+        NormalEnvironment("ざわざわ環境") {
+            it.noise > 120.0 &&
+                    it.light > 200.0 && it.tvoc > 100
         } ,
-        NormalEnvironment("高温度×高湿度環境") {
-            it.temperature > 28 &&
-                    it.humidity > 70.0
+        NormalEnvironment("暗い静か環境") {
+            it.light < 80 &&
+                    it.noise < 70.0
         },
         /*NormalEnvironment("チェック用") {
                     it.humidity > 70.0
